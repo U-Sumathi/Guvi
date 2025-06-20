46d5fcc5786dbfcf324b1374007fd432ff50ad49
Sumathi <156941464+U-Sumathi@users.noreply.github.com>
2025-06-20 13:43:57 +0530

    updated

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("wecome");
+
+/* alert("if u click ok ...then loading  gone"); */
+
+
+/* let namy=prompt("enter:");  *//*  if you enter name loading gone */
+ /*console.log(namy);*/  /*  see ur enter data in console */
+
+
+let names=confirm("click ok"); /*  if you click ok ..then loading gone */
+ console.log(names);        /*  o/p==>true or false */
+
+
+
+ let z;
+ console.log(z);
+console.log(typeof(a));
+
+let c=null;
+console.log(c);
+console.log(typeof(c));
+
+let area={
+    areaname : "kpm"
+}
+console.log(typeof(area))    //o/p=>datatype is object
